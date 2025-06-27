fe783d39c1a6d33e81d35a41b2580a4b0175076b
Untitled <untitled@Untitleds-MacBook-Air.local>
2025-06-27 15:35:59 +0900

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name = "MEDICAL BOT AI PROJECT",
+    version = "0.0.0",
+    author = "min",
+    author_email = "kwonmobin3@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
